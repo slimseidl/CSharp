8a20b24ab69d567d770d5d41b7233117f9260244
John Seidl <jseidl2@wgu.edu>
2025-07-07 14:56:38 -0500

    Made Updates

--- 03/demos/end/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ 03/demos/end/BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -2,31 +2,32 @@
 //Console.WriteLine("Hello, World!");
 
 
-Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
-//Console.WriteLine("Enter your name:");
+// Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+// //Console.WriteLine("Enter your name:");
 
-//The following will accept the name
-//string name = Console.ReadLine();
+// //The following will accept the name
+// //string name = Console.ReadLine();
 
-/*
-    Here are some tests around working with valid identifiers in C#
-    We can write here as much as we want, this is all comment
-*/
+// /*
+//     Here are some tests around working with valid identifiers in C#
+//     We can write here as much as we want, this is all comment
+// */
 
-//string name2 = Console.ReadLine();
-//string name_2 = Console.ReadLine();
-//string Name_2 = Console.ReadLine();
+// //string name2 = Console.ReadLine();
+// // string name_2 = Console.ReadLine();
+// // string Name_2 = Console.ReadLine();
 
-//string 2name = Console.ReadLine();
+// // string 2name = Console.ReadLine();
 
- 
-var monthlyWage = 1234;
-int months = 12, bonus = 1000;
+//  // Can add commas to declare multiple vairables on one line
+// var monthlyWage = 1234;
+// int months = 12, bonus = 1000;
 
 
-bool isActive = true;
-double rating = 99.25;
-//byte numberOfEmployees = 300;
+// bool isActive = true;
+// double rating = 99.25;
+// // byte numberOfEmployees = 300;
+// // byte cannot be > 255
 
 int hoursWorked;
 
@@ -34,21 +35,26 @@ hoursWorked = 125;
 
 hoursWorked = 148;
 
-//monthlyWage = true;
+//monthlyWage = true; already initialized as an int 
 
-const double interestRate = 0.07;
-//interestRate = 0.08;
+// // const = constant = variable will NEVER change
+// const double interestRate = 0.07;
+// //interestRate = 0.08; compiler flags the error
+
+// // Creating a string
+// string firstName = "Bethany";
+// string lastName = "Smith";
+
+// string emptyString = "";
+
+// string s;
+
+// s = firstName;
+
+int bonus = 1000;
 
 
-string firstName = "Bethany";
-string lastName = "Smith";
-
-string emptyString = "";
-
-string s;
-
-s = firstName;
-
+// Operators
 double ratePerHour = 12.34;
 int numberOfHoursWorked = 165;
 
@@ -68,45 +74,45 @@ Console.WriteLine(numberOfEmployees);
 
 
 
-int intMaxValue = int.MaxValue;
-int intMinValue = int.MinValue;
+// int intMaxValue = int.MaxValue;
+// int intMinValue = int.MinValue;
 
-char userSelection = 'a';
+// char userSelection = 'a';
 
-char upperVersion = char.ToUpper(userSelection);
+// char upperVersion = char.ToUpper(userSelection);
 
-bool isDigit = char.IsDigit(userSelection);
+// bool isDigit = char.IsDigit(userSelection);
 
-bool isLetter = char.IsLetter(userSelection);
+// bool isLetter = char.IsLetter(userSelection);
 
-DateTime hireDate = new DateTime(2022, 3, 28, 14, 30, 0);
-Console.WriteLine(hireDate);
+// DateTime hireDate = new DateTime(2022, 3, 28, 14, 30, 0);
+// Console.WriteLine(hireDate);
 
-DateTime exitDate = new DateTime(2025, 12, 11);
+// DateTime exitDate = new DateTime(2025, 12, 11);
 
-//DateTime invalidDate = new DateTime(2025, 15, 11);
+// //DateTime invalidDate = new DateTime(2025, 15, 11);
 
-DateTime startDate = hireDate.AddDays(15);
-Console.WriteLine(startDate);
+// DateTime startDate = hireDate.AddDays(15);
+// Console.WriteLine(startDate);
 
-var currentDate = DateTime.Now;
-bool areWeInDst = currentDate.IsDaylightSavingTime();
+// var currentDate = DateTime.Now;
+// bool areWeInDst = currentDate.IsDaylightSavingTime();
 
-DateTime startHour = DateTime.Now;
-TimeSpan workTime = new TimeSpan(8, 35, 0);
-DateTime endHour = startHour.Add(workTime);
+// DateTime startHour = DateTime.Now;
+// TimeSpan workTime = new TimeSpan(8, 35, 0);
+// DateTime endHour = startHour.Add(workTime);
 
-Console.WriteLine(startHour);
-Console.WriteLine(endHour);
+// Console.WriteLine(startHour);
+// Console.WriteLine(endHour);
 
-Console.WriteLine(startHour.ToLongDateString());
-Console.WriteLine(endHour.ToShortTimeString());
+// Console.WriteLine(startHour.ToLongDateString());
+// Console.WriteLine(endHour.ToShortTimeString());
 
-long veryLongMonth = numberOfHoursWorked;//works fine!
+// long veryLongMonth = numberOfHoursWorked;//works fine!
 
-double d = 123456789.0;
-int x = (int)d;
+// double d = 123456789.0;
+// int x = (int)d;
 
-int intVeryLongMonth = (int)veryLongMonth;//might fail
+// int intVeryLongMonth = (int)veryLongMonth;//might fail
 
-Console.ReadLine();
+// Console.ReadLine();
---
+++ scratch/Basic.cs
@@ -0,0 +1,9 @@
+Console.WriteLine("Welcome to the Pie Shop");
+int a = 2;
+int b = a + 3;
+
+bool c = true;
+
+// Data types cannot be changed after initialized variable
+int c = 3; 
+
