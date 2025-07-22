69ab506551ad27b65dd63de4b48e0b3151437edf
John Seidl <jseidl2@wgu.edu>
2025-07-22 13:49:29 -0500

    Loops pracitce

--- 06_Loops/Start/Program.cs
+++ 06_Loops/Start/Program.cs
@@ -1,7 +1,7 @@
 ﻿
-using BethanysPieShopHRM;
+//using BethanysPieShopHRM;
 
-Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+//Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
 
 
 //int amount = 1234;
@@ -20,9 +20,103 @@ Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
 
 //double yearlyWageWithBonusDouble = Utilities.CalculateYearlyWage(amountDouble, monthsDouble, bonusDouble);
 
-Utilities.UsingOptionalParameters();
+//Utilities.UsingOptionalParameters();
 
-Utilities.UsingNamedArguments();
+//Utilities.UsingNamedArguments();
+
+// Basic loop
+//int i = 0;
+//while (i < 10) 
+//{
+//    Console.WriteLine(i);
+//    i++;
+//}
+
+//// While i is less than value input given
+//Console.WriteLine("Enter a value: ");
+//int max = int.Parse(Console.ReadLine());
+//int i = 0;
+//while (i < max)
+//{
+//    Console.WriteLine(i);
+//    i++;
+//}
+
+//Console.WriteLine("Loop Complete");
+
+// Loop around switch statement, makes user input a value to exit options
+//Console.WriteLine("Choose from the following menu: ");
+//Console.WriteLine("1. Add employee");
+//Console.WriteLine("2. Update employee");
+//Console.WriteLine("3. Delete employee");
+//string selectedAction = Console.ReadLine();
+
+//while (selectedAction != "99")
+//{
+//    switch (selectedAction)
+//    {
+//        case "1":
+//            Console.WriteLine("Adding new empployee to database");
+//            break;
+//        case "2":
+//            Console.WriteLine("Updating employee in database");
+//            break;
+//        case "3":
+//            Console.WriteLine("Deleting employee");
+//            break;
+//        default:
+//            Console.WriteLine("Invalid selection");
+//            break;
+//    }
+//    Console.WriteLine("Choose from the following menu: ");
+//    Console.WriteLine("1. Add employee");
+//    Console.WriteLine("2. Update employee");
+//    Console.WriteLine("3. Delete employee");
+//    Console.WriteLine("99. Exit Menu");
+//    selectedAction = Console.ReadLine();
+//}
+//Console.WriteLine("Closing application");
+
+// For Loops
+// for (initialize, boolean)
+// for (int i =0, i < 10; i++)
+
+//do While loops
+//string selectedAction;
+//do
+//{
+//    Console.WriteLine("Choose from the following menu: ");
+//    Console.WriteLine("1. Add employee");
+//    Console.WriteLine("2. Update employee");
+//    Console.WriteLine("3. Delete employee");
+//    selectedAction = Console.ReadLine();
+
+
+//    switch (selectedAction)
+//    {
+//        case "1":
+//            Console.WriteLine("Adding new empployee to database");
+//            break;
+//        case "2":
+//            Console.WriteLine("Updating employee in database");
+//            break;
+//        case "3":
+//            Console.WriteLine("Deleting employee");
+//            break;
+//        default:
+//            Console.WriteLine("Invalid selection");
+//            break;
+//    }
+//} while (selectedAction != "99");
+//Console.WriteLine("Closing application");
+
+
+// For loops 
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
 
 Console.ReadLine();
 
--- 06_Loops/Start/obj/Debug/net8.0/BethanysPieShopHRM.AssemblyInfo.cs
+++ 06_Loops/Start/obj/Debug/net8.0/BethanysPieShopHRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShopHRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+550c19ae2ac81f599af65a3dcbe39a7dd7a1ecc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29dd533e261cd767a87ccfca950fc6e27ae6c450")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanysPieShopHRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanysPieShopHRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
