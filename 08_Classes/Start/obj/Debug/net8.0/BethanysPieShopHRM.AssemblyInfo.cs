128af5a0036ce4a15f08e07a719eb422526b30f5
John Seidl <jseidl2@wgu.edu>
2025-07-28 09:14:31 -0500

    Final Class

--- 08_Classes/Start/obj/Debug/net8.0/BethanysPieShopHRM.AssemblyInfo.cs
+++ 08_Classes/Start/obj/Debug/net8.0/BethanysPieShopHRM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanysPieShopHRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1edf5d134ffd3ed197ab6490db29feabae5f235")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e12470f096423fd2775577d72d2c82b9f9f31720")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanysPieShopHRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanysPieShopHRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
