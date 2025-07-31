e8c00c01796e887c9e22dd53c821f0b442c56f11
John Seidl <jseidl2@wgu.edu>
2025-07-31 09:37:01 -0500

    Conditionals practice

---
+++ UdemyConditionals/ConditionExercises/Validation/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Please enter a number between 1 and 10.");
+
+int numEntry = int.Parse(Console.ReadLine());
+
+if (numEntry > 0 && numEntry <= 10)
+{
+    Console.WriteLine("Valid entry.");
+}
+else
+{
+    Console.WriteLine("Invalid entry");
+}
+
+Console.WriteLine("Press any key to close.....");
+Console.ReadLine();
